b9372a6b1633965f94a2cb2b8a44be784c366ec4
Noah Ramey and Sebastian Prokuski <sebastian@>
2016-08-24 11:12:33 -0700

    add cart functionality

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('drone', {path: '/drone/:drone_id'});
+  this.route('cart');
 });
 
 export default Router;
--- app/routes/drone.js
+++ app/routes/drone.js
@@ -1,7 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  shoppingCart: Ember.inject.service(),
   model(params) {
     return this.store.findRecord('drone', params.drone_id);
+  },
+  actions: {
+    addToCart(item) {
+      this.get('shoppingCart').add(item);
+    }
   }
 });
