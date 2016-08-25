1d3e54828649b019965c542fb52fd6a4bce3b5a9
Noah Ramey and Sebastian Prokuski <sebastian@>
2016-08-25 10:13:12 -0700

    display average reviews using a helper and firebase query

--- app/helpers/avg-review.js
+++ app/helpers/avg-review.js
@@ -2,17 +2,35 @@ import Ember from 'ember';
 
 export function avgReview(params) {
   var reviews = params[0];
-  var numberOfReviews = reviews.content.length - 1;
+  var numberOfReviews = reviews.content.length;
   var totalOfRatings = 0;
 
-  console.log(reviews);
-// NEED TO GET TO REVIEWS CONTENT THROUGH DRONE OBJECT
-  for (var i = 0; i < reviews.content.length; i++) {
-    var ratingInt = parseInt(reviews[i].get('rating'));
-    console.log(ratingInt);
-    totalOfRatings += ratingInt;
+  if (numberOfReviews === 0) {
+    return "No Reviews for this Product.";
+  } else {
+    for (var i = 0; i < reviews.content.length; i++) {
+      console.log(reviews.content[i]._data.rating);
+      var ratingInt = parseInt(reviews.content[i]._data.rating);
+      totalOfRatings += ratingInt;
+    }
+    var average = totalOfRatings / numberOfReviews;
+    average = Math.round(average);
+    if (average === 1) {
+      return average = "\u2605";
+    } else if (average === 2) {
+      return average = "\u2605\u2605";
+    } else if (average === 3) {
+      return average = "\u2605\u2605\u2605";
+    } else if (average === 4) {
+      return average = "\u2605\u2605\u2605\u2605";
+    } else if (average === 5) {
+      return average = "\u2605\u2605\u2605\u2605\u2605";
+    }
+    return average;
   }
-  return (totalOfRatings / numberOfReviews);
+
+
+
 }
 
 export default Ember.Helper.helper(avgReview);
--- app/routes/drone.js
+++ app/routes/drone.js
@@ -3,20 +3,31 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   shoppingCart: Ember.inject.service(),
   model(params) {
-    return this.store.findRecord('drone', params.drone_id);
+    return Ember.RSVP.hash({
+      drone: this.store.findRecord('drone', params.drone_id),
+      reviews: this.store.query('review', {
+        orderBy: 'drone',
+        equalTo: params.drone_id
+      })
+    });
   },
   actions: {
     addToCart(item) {
       this.get('shoppingCart').add(item);
     },
+    transitionToCart() {
+      this.transitionTo('cart');
+    },
     saveReview(params) {
+      var self = this;
       var newReview = this.store.createRecord('review', params);
       var drone = params.drone;
       drone.get('reviews').addObject(newReview);
       newReview.save().then(function() {
         return drone.save();
+      }).then(function() {
+        return self.refresh();
       });
-      this.transitionTo('drone', drone.id);
     }
   }
 });
